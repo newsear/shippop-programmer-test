56502e6c85639368870a2c47c88a2c3caefdac5d
ccgame <wachirapol@knstrans.com>
2020-08-28 17:18:57 +0700

    add state and simple event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Typography, Input, Select, Button, List } from "antd";
 
@@ -42,32 +42,63 @@ const StyledList = styled(List)`
 `;
 
 function App() {
+  const [state, setState] = useState({
+    list: "",
+    search: "",
+    type: "linear_search",
+    data: [],
+  });
+
+  const onInputChange = (keyName, value) => {
+    setState({ ...state, [keyName]: value });
+  };
+
+  const onSelectChange = (value) => {
+    setState({ ...state, type: value });
+  };
+
+  const onButtonClick = () => {
+    console.log("state", state);
+  };
+
   return (
     <Container>
       <Row>
-        <Label sty>List</Label>
-        <StyledInput />
+        <Label>List</Label>
+        <StyledInput
+          value={state.list}
+          onChange={(e) => onInputChange("list", e.currentTarget.value)}
+        />
       </Row>
       <Row>
         <Label>Search</Label>
-        <StyledInput />
+        <StyledInput
+          value={state.search}
+          onChange={(e) => onInputChange("search", e.currentTarget.value)}
+        />
       </Row>
       <Row>
         <Label>Search Type</Label>
-        <StyledSelect defaultValue="linear_search">
+        <StyledSelect value={state.type} onChange={onSelectChange}>
           <Option value="linear_search">Linear Search</Option>
           <Option value="binary_search">Binary Search</Option>
           <Option value="bubble_sort">Bubble Sort</Option>
         </StyledSelect>
       </Row>
       <Row>
-        <StyledButton type="primary">Search</StyledButton>
+        <StyledButton
+          type="primary"
+          disabled={!state.list || !state.search}
+          onClick={onButtonClick}
+        >
+          Search
+        </StyledButton>
       </Row>
       <Spacer />
       <StyledList
         header={<Label>Result</Label>}
         bordered
-        dataSource={["data"]}
+        dataSource={state.data}
         renderItem={(item) => (
           <Item>
             <Text>{item}</Text>
